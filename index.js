1c7e31767c6c98c9b338c9cb85bf4579cbc60e40
Jorge Cortes <jcortes@belatrixsf.com>
2018-02-07 17:24:25 -0500

    addded proxy requests

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-require("dotenv").config();
+const { config } = require("dotenv");
+const path = require("path");
+const express = require("express");
+const proxy = require("express-http-proxy");
 
+config();
 console.log(process.env.CONFIG_VAR);
+
+const app = express();
+
+const baseImgUrl = process.env.BASE_IMG_URL;
+const dir = path.join(__dirname, "public/images");
+
+const proxyOptions = {
+  proxyReqPathResolver: req => {
+    const newPath = `${baseImgUrl}${req.path}`;
+    console.log(newPath)
+    return newPath;
+  }
+};
+
+const handler = baseImgUrl
+  ? proxy(baseImgUrl, proxyOptions)
+  : express.static(dir);
+
+app.use("/images", handler);
+
+app.listen(8080);
