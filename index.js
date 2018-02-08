3343506b25f8145ec3071136f91e2e2fabef21db
Jorge Cortes <jcortes@belatrixsf.com>
2018-02-08 14:00:47 -0500

    created new code to handle file uploads

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { config } = require("dotenv");
 const path = require("path");
 const express = require("express");
 const proxy = require("express-http-proxy");
+const fileUpload = require("express-fileupload");
 
 config();
 console.log(process.env.CONFIG_VAR);
@@ -9,7 +10,8 @@ console.log(process.env.CONFIG_VAR);
 const app = express();
 
 const baseImgUrl = process.env.BASE_IMG_URL;
-const dir = path.join(__dirname, "public/images");
+const imagesDir = path.join(__dirname, "public/images");
+const uploadsDir = path.join(__dirname, "uploads");
 
 const proxyOptions = {
   proxyReqPathResolver: req => {
@@ -19,12 +21,46 @@ const proxyOptions = {
   }
 };
 
-const handler = baseImgUrl
+const imageHandler = baseImgUrl
   ? proxy(baseImgUrl, proxyOptions)
-  : express.static(dir);
+  : express.static(imagesDir);
+
+const uploadsHandler = express.static(uploadsDir);
+
+const form = `
+  <form action="/upload" enctype="miltipart/form-data" method="post">
+    <input type="file" name="foo" /> <br><br>
+    <input type="submit" value="Upload" />
+  </form>
+`;
+
+const formHandler = (req, { send }) => {
+  send(form);
+};
+
+const uploadHandler = ({ files }, { status, send }) => {
+  if (!files) {
+    return status(400).send("No files were uploaded!");
+  }
+  const { foo } = files;
+  const uploadTo = `uploads/${foo.name}`;
+  foo.mv(uploadTo, err => {
+    if (err) {
+      return status(500).send(err);
+    }
+    const link = `<a href="${uploadTo}">${uploadTo}</a>`;
+    send(`File uploaded to ${link}`);
+  });
+};
 
 // BASE_IMG_URL=https://cdn.pixabay.com/photo/2017/10/12/20/12
 // abstract-2845763_960_720.jpg
-app.use("/images", handler);
+app.use("/images", imageHandler);
+app.use(fileUpload());
+app.use("/uploads", uploadsHandler);
+app.get("/", formHandler);
+app.post("/upload", uploadHandler);
 
-app.listen(8080);
+const server = app.listen(8080, () => {
+  console.log(`Server listening on port ${server.address().port}`);
+});
